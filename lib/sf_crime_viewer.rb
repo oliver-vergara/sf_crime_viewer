11e7adaebd95d289b436a33d151ebc55934be7d0
Oliver <deftool@outlook.com>
2016-06-06 20:28:57 -0700

    created Viewer class

--- lib/sf_crime_viewer.rb
+++ lib/sf_crime_viewer.rb
@@ -1,5 +1,27 @@
 require "sf_crime_viewer/version"
+require "unirest"
 
 module SfCrimeViewer
-  # Your code goes here...
+  class Viewer
+    
+    attr_reader :category, :address, :date, :time, :description, :resolution
+
+    def initialize(hash)
+      @category = hash["category"]
+      @address = hash["address"]
+      @date = hash["date"]
+      @time = hash["time"]
+      @description = hash["descript"]
+      @resolution = hash["resolution"]
+    end
+
+    def self.all 
+      crimes_array = Unirest.get("https://data.sfgov.org/resource/gxxq-x39z.json").body
+      crimes = []
+      crimes_array.each do |crime_hash|
+        crimes << Viewer.new(crime_hash)
+      end
+      return crimes
+    end
+  end
 end
